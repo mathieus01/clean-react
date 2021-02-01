d0c4277ab8a247e99081896235cc24121547a8f3
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-01 20:48:17 -0300

    feat: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class EmailValidation implements FieldValidation {
+  constructor (readonly fieldName: string) {}
+  validate (value: string): Error {
+    return new InvalidFieldError(this.fieldName)
+  }
+}
