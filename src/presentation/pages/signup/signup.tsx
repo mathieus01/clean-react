63d2a58aa9a66f40a31f736e661953afadd3e066
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-09 08:00:37 -0300

    feat: ensure SignUp enable submit button if form is valid

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -41,7 +41,10 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
           <Input type="email" name="email" placeholder="Digite seu e-mail"/>
           <Input type="password" name="password" placeholder="Digite seu senha"/>
           <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha"/>
-          <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
+          <button data-testid="submit"
+            className={Styles.submit}
+            type="submit"
+            disabled={!!state.nameError || !!state.emailError || !!state.passwordError || !!state.passwordConfirmationError}>Entrar</button>
           <span className={Styles.link}>Voltar para Login</span>
           <FormStatus />
         </form>
