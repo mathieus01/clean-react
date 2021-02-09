c74ac9f93b5d3d3c3599785677c76042a419dc10
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-09 14:23:21 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  return new ValidationComposite([
+    ...Builder.field('name').required().min(3).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').min(5).required().build(),
+    ...Builder.field('passwordConfirmation').min(5).required().sameAs('password').build()
+  ])
+}
