e4af377ae3da7e5da61b6c7deb4b118b1ebb5adc
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-09 15:19:20 -0300

    feat: add SignUp Composition

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -9,7 +9,7 @@ describe('SignUpValidationFactory', () => {
       ...Builder.field('name').required().min(3).build(),
       ...Builder.field('email').required().email().build(),
       ...Builder.field('password').min(5).required().build(),
-      ...Builder.field('passwordConfirmation').min(5).required().sameAs('password').build()
+      ...Builder.field('passwordConfirmation').required().sameAs('password').build()
     ]))
   })
 })
--- src/main/factories/pages/signup/signup-validation-factory.ts
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -6,6 +6,6 @@ export const makeSignUpValidation = (): ValidationComposite => {
     ...Builder.field('name').required().min(3).build(),
     ...Builder.field('email').required().email().build(),
     ...Builder.field('password').min(5).required().build(),
-    ...Builder.field('passwordConfirmation').min(5).required().sameAs('password').build()
+    ...Builder.field('passwordConfirmation').required().sameAs('password').build()
   ])
 }
