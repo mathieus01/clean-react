b13aa670b113b111e150e79740365619de239d4f
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-11 09:08:37 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -20,10 +20,14 @@ export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   }
 }
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<R> implements HttpGetClient<R> {
   url: string
-  async get (params: HttpGetParams): Promise<void> {
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async get (params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url
-    return Promise.resolve()
+    return Promise.resolve(this.response)
   }
 }
