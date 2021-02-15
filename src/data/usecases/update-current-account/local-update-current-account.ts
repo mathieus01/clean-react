ca37ae450a49bd6483e5ce9cfef81330202c5e69
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-15 12:46:06 -0300

    refactor: change cache storage to sync

--- src/data/usecases/update-current-account/local-update-current-account.ts
+++ src/data/usecases/update-current-account/local-update-current-account.ts
@@ -10,6 +10,7 @@ export class LocalUpdateCurrentAccount implements UpdateCurrentAccount {
     if (!account?.accessToken) {
       throw new UnexpectedError()
     }
-    await this.setStorage.set('account', JSON.stringify(account))
+    this.setStorage.set('account', JSON.stringify(account))
+    return Promise.resolve()
   }
 }
--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,4 +1,3 @@
-import { SetStorage } from '@/data/protocols/cache/set-storage'
 import faker from 'faker'
 import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from './local-storage-adapter'
@@ -9,11 +8,11 @@ describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     localStorage.clear()
   })
-  test('Should call localStorage with correct values', async () => {
+  test('Should call localStorage with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
-    await sut.set(key, value)
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
 })
