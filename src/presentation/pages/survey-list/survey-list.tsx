80afd99c06e6a6f1bae88104b1ba82c87061f9a0
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-15 08:55:59 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,6 +21,54 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>12</span>
+                <span className={Styles.month}>02</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é seu framework web favorito ?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>12</span>
+                <span className={Styles.month}>02</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é seu framework web favorito ?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>12</span>
+                <span className={Styles.month}>02</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é seu framework web favorito ?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>12</span>
+                <span className={Styles.month}>02</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é seu framework web favorito ?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
         </ul>
       </div>
       <Footer />
