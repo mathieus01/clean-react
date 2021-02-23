3e235eb32dea1dd617f4c0311e6ebba010c4c843
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-23 08:01:56 -0300

    feat: ensure RemoteLoadSurveyResult throws UnexpectedError on 404

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,5 +1,5 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { LoadSurveyResult } from '@/domain/usecases'
 
 export class RemoteLoadSurveyResult implements LoadSurveyResult {
@@ -12,7 +12,8 @@ export class RemoteLoadSurveyResult implements LoadSurveyResult {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: break
-      default: throw new AccessDeniedError()
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: throw new UnexpectedError()
     }
     return Promise.resolve(null)
   }
