2e55c7b885c4cd0ced5631719a2baec9f2225c61
Matheus Nunes <mathieusnunes@gmail.com>
2021-02-27 16:57:06 -0300

    chore: add dev webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,10 @@
-const path = require('path')
 const { DefinePlugin } = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'main-bundle-[hash].js',
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
   module: {
     rules: [{
       test: /\.ts(x?)$/,
@@ -35,20 +24,19 @@ module.exports = {
       }]
     }]
   },
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: './public',
     writeToDisk: true,
     historyApiFallback: true,
     port: 8080
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
+    }),
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
     })
   ]
-}
+})
